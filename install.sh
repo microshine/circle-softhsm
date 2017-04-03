6dc23ad49874d549c55e0253458e792aaf459456
microshine <microshine@mail.ru>
2017-04-03 16:04:04 +0300

    Update

--- install.sh
+++ install.sh
@@ -2,12 +2,21 @@
 set -e
     
 # Installing SoftHSM dependencies
+echo
+echo "Install dependencies..."
+echo
 sudo apt-get update -qq
 sudo apt-get install libssl-dev
 sudo apt-get install build-essential autoconf automake libtool
-sudo libtoolize
+echo
+echo "Add missing..."
+echo
+sudo libtoolize && sudo aclocal && sudo autoheader && sudo autoconf && sudo automake --add-missing
 
 # Installing SoftHSM
+echo
+echo "Installing SoftHSM..."
+echo
 git clone https://github.com/opendnssec/SoftHSMv2.git -b master --depth 1
 cd SoftHSMv2
 sh ./autogen.sh
